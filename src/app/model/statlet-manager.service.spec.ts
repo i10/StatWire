f2f40696ca4bacf37be24c647aea06e1fc90f5d8
Y0hy0h <y0hy0h@users.noreply.github.com>
2017-06-29 17:29:21 +0200

    Add isolated tests for `StatletManager` service

--- src/app/model/statlet-manager.service.spec.ts
+++ src/app/model/statlet-manager.service.spec.ts
@@ -1,15 +1,53 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { CanvasPosition } from './canvas-position';
 import { StatletManagerService } from './statlet-manager.service';
 
 describe('StatletManagerService', () => {
+  let statletManager: StatletManagerService;
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [StatletManagerService]
-    });
+    statletManager = new StatletManagerService();
   });
 
-  it('should be created', inject([StatletManagerService], (service: StatletManagerService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('#createStatlet should save a Statlet when one is created', () => {
+    const statlet = statletManager.createStatlet(
+      'saveMe',
+      new CanvasPosition(10, 10),
+    );
+    const allStatlets = statletManager.getAllStatlets();
+    expect(allStatlets).toContain(statlet);
+  });
+
+  it('#getStatlet should return undefined when no matching Statlet is found', () => {
+    const result = statletManager.getStatlet(1);
+    expect(result).toBeUndefined();
+  });
+
+  describe('with added statlets', () => {
+    let statlet1, statlet2;
+    beforeEach(() => {
+      statlet1 = statletManager.createStatlet(
+        'first',
+        new CanvasPosition(10, 10),
+      );
+      statlet2 = statletManager.createStatlet(
+        'second',
+        new CanvasPosition(10, 100),
+      );
+    });
+
+    it('#createStatlet should assign different ids to two created Statlets', () => {
+      expect(statlet1.id).not.toEqual(statlet2.id);
+    });
+
+    it('should retrieve all created nodes', () => {
+      const allStatlets = statletManager.getAllStatlets();
+      expect(allStatlets).toEqual([statlet1, statlet2]);
+    });
+
+    it('#getStatlet should return the Statlet with the right id', () => {
+      const actual1 = statletManager.getStatlet(statlet1.id);
+      expect(actual1).toBe(statlet1);
+      const actual2 = statletManager.getStatlet(statlet2.id);
+      expect(actual2).toBe(statlet2);
+    });
+  });
 });
