136b4318ae00de909dfdee09abbecfcb0527908f
Y0hy0h <y0hy0h@users.noreply.github.com>
2017-07-16 15:31:06 +0200

    Remove redundant `RemoteRService` provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
-import { RemoteRService } from './remote-r.service';
-
 @Component({
   selector: 'sl-root',
   templateUrl: './app.component.html',
-  providers: [RemoteRService],
 })
 export class AppComponent { }
