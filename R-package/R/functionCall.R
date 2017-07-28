233627944aaf694bb57c2a6be8d0bff9d1b166f8
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-07-28 15:02:36 +0200

    Adapt custom R-package's function to serialize and unserialize S3 objects automatically

--- R-package/R/functionCall.R
+++ R-package/R/functionCall.R
@@ -1,3 +1,41 @@
+library(jsonlite)
+
+serializeS3SingleValue <- function(val) {
+  serializedVal <- tryCatch({
+    toJSON(val)
+    val
+  }, error = function(e) {
+    serializeJSON(val)
+  })
+  return(serializedVal)
+}
+
+serializeS3Values <- function(values) {
+  serializedValues <- lapply(values, serializeS3SingleValue)
+  return(serializedValues)
+}
+
+unserializeS3SingleValue <- function(val) {
+  charVal <- paste(val)
+  unserializedVal <- tryCatch({
+    unserializeJSON(charVal)
+  }, error = function(e) {
+    val
+  })
+  return(unserializedVal)
+}
+
+unserializeS3Values <- function(values) {
+  unserializedValues <- lapply(values, unserializeS3SingleValue)
+  return(unserializedValues)
+}
+
 functionCall <- function(func, ...) {
-  invisible(do.call(func, list(...)))
+  inputs <- list(...)
+  unserializedInputs <- unserializeS3Values(inputs)
+
+  returnValues <- do.call(func, unserializedInputs)
+
+  serializedReturnValues <- serializeS3Values(returnValues)
+  r(serializedReturnValues)
 }
