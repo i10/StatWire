ea90bbfbf87e7146d66d58dbe6d39418e1ee7178
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-08-01 12:44:55 +0200

    Make `StatletManager` manage `StatletGroup`s

--- src/app/model/statlet-manager.service.ts
+++ src/app/model/statlet-manager.service.ts
@@ -4,10 +4,12 @@ import { RemoteRService } from '../remote-r.service';
 import { CanvasPosition } from './canvas-position';
 import { Parameter } from './parameter';
 import { Statlet } from './statlet';
+import { StatletGroup } from 'app/model/statletGroup';
 
 @Injectable()
 export class StatletManagerService {
-  allStatlets: Statlet[] = [];
+  allStatlets: Array<Statlet> = [];
+  allGroups: Array<StatletGroup> = [];
   activeStatlet: Statlet;
 
   private nextStatletId = 1;
@@ -15,6 +17,23 @@ export class StatletManagerService {
   constructor(
     private remoteR: RemoteRService
   ) { }
+
+  createGroup(position: CanvasPosition): StatletGroup {
+    const id = this.nextStatletId;
+    this.nextStatletId++;
+
+    const group = new StatletGroup(
+      id,
+      position,
+    );
+
+    this.addGroup(group);
+    return group;
+  }
+
+  private addGroup(group: StatletGroup): void {
+    this.allGroups.push(group);
+  }
 
   createStatlet(position: CanvasPosition): Statlet {
     const id = this.nextStatletId;
