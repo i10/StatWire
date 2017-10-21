c9238192625c78f936fbeb91fe107764f43110ec
Y0hy0h <y0hy0h@users.noreply.github.com>
2017-10-21 20:21:01 +0200

    Add failing test for `RemoteRService` for the passing of arguments between calls

---
+++ src/app/remote-r.service.spec.ts
@@ -0,0 +1,37 @@
+import { RemoteRService } from './remote-r.service';
+
+describe('RemoteRService @OpenCPU integration', () => {
+  it('should be able to contact the OpenCPU server', (done) => {
+    fetch(RemoteRService.packageUrl, {headers: {'Cache-Control': 'no-cache'}})
+      .then(response => {
+        if (response.ok) {
+          return;
+        } else {
+          throw new TypeError(`Expected response code to be ok, was ${response.status}.`);
+        }
+      })
+      .catch(fail)
+      .then(done);
+  });
+
+  it('should get same print statement when passing summary object', (done) => {
+    const firstFunction = 'function() {\n  data = data.frame(x=c(1,2),y=c(3,4))\n'
+      + '  data$y = factor(data$y)\n  print(summary(data))\n	return(data)\n}';
+    const secondFunction = `function(data) {\n  print(summary(data))\n  return()\n}`;
+    const remoteR = new RemoteRService();
+    remoteR.initializationPromise
+      .then(() => {
+        return remoteR.execute(firstFunction, {argsToEvaluate: []});
+      })
+      .then(firstResult => {
+        const data = firstResult.returnValue[0];
+        const argsObject = {data: data, argsToEvaluate: []};
+        return remoteR.execute(secondFunction, argsObject)
+          .then(secondResult => {
+            expect(firstResult.consoleOutput).toEqual(secondResult.consoleOutput);
+          });
+      })
+      .catch(fail)
+      .then(done);
+  });
+});
